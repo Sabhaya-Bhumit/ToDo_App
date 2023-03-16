8b10937f054ce7bbb1f0d1d7b2e54fee3817c3e0
bhumit sabhaya <sabhayabhumit@gmail.com>
2023-03-17 00:42:22 +0530

    TODO APP

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+// import 'package:color_panel/view/home.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:sizer/sizer.dart';
+import 'package:to_do_app/views/screens/home_page.dart';
+// import 'package:pr3in2/homephase.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Sizer(
+      builder: (context, orientation, deviceType) {
+        return GetMaterialApp(
+          debugShowCheckedModeBanner: false,
+          home: HomePage(),
+        );
+      },
+    );
+  }
+}
